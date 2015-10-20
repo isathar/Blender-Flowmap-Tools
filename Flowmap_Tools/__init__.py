b44ac83b009e7ad74707bac1ae329736ede3d484
Andreas Wiehn <isathar@gmail.com>
2015-10-19 22:15:58 -0400

    update 0.5
    - fixed particle sorting problem
    - added ability to calculate flowmaps from velocities + directions

--- Flowmap_Tools/__init__.py
+++ Flowmap_Tools/__init__.py
@@ -19,7 +19,7 @@
 bl_info = {
 	"name": "Flowmap Tools",
 	"author": "Andreas Wiehn (isathar)",
-	"version": (0, 0, 1),
+	"version": (0, 0, 5),
 	"blender": (2, 74, 0),
 	"location": "View3D > Toolbar",
 	"description": "Editing tools for flowmaps",
@@ -31,6 +31,7 @@ bl_info = {
 
 import bpy
 from mathutils import Vector
+from . import flowmap_data
 
 
 # UI Panel
@@ -52,19 +53,49 @@ class flowmaps_editor_panel(bpy.types.Panel):
 		# Create
 		layout = self.layout
 		box = layout.box()
-		box.row().operator('object.create_flowmap_field',text='Create')
-		box.row().operator('object.calc_flowmap_velocicites',text='Calculate')
-		box.row().operator('object.blur_flowmap_field',text='Blur')
+		box.row().operator('object.create_vectorfield_2d',text='Create Data')
+		
+		box2 = layout.box()
+		box = box2.box()
+		box.row().operator('object.calc_flowmap_dir',text='Calc Direction')
+		box.row().prop(context.window_manager, 'flowmap_cache0weight',text='Weight')
+		
+		box = box2.box()
+		box.row().operator('object.calc_flowmap_velocities',text='Calc Forces')
+		box.row().prop(context.window_manager, 'flowmap_cache1weight',text='Weight')
+		
+		box = box2.box()
+		box.row().operator('object.calc_flowmap_geometry',text='Calc Geometry')
+		box.row().prop(context.window_manager, 'flowmap_cache2weight',text='Weight')
+		
+		box = layout.box()
+		box.row().prop(context.window_manager, 'flowmap_usebchan',text='Blue Channel')
+		box.row().operator('object.flowmap_writetocolor',text='Write to Color')
+		
 		
 		
 
-# creates particle system on selected plane to represent flowmap vectors
-class create_flowmap_field(bpy.types.Operator):
-	bl_idname = 'object.create_flowmap_field'
-	bl_label = 'Create Flowmap Particles'
-	bl_description = 'Turn plane into flowfield'
+
+## Init ####################################################
+
+def get_nearest_vindex(tempverts, checkloc):
+	lastdist = 8192.0
+	curindex = -1
+	
+	for i in range(len(tempverts)):
+		testmag = (tempverts[i] - checkloc).magnitude
+		if testmag <= lastdist:
+			lastdist = testmag
+			curindex = i
+	return curindex
+
+# creates particle system in selected object to represent flowmap vectors
+class create_vectorfield_2d(bpy.types.Operator):
+	bl_idname = 'object.create_vectorfield_2d'
+	bl_label = 'Create Flowmap Data'
+	bl_description = 'Create flow particles for selected object'
 	bl_options = {'REGISTER', 'UNDO'}
-
+	
 	@classmethod
 	def poll(cls, context):
 		return (context.mode == "OBJECT" and context.active_object != None)
@@ -72,9 +103,10 @@ class create_flowmap_field(bpy.types.Operator):
 	def execute(self, context):
 		flowmesh = context.active_object
 		numverts = len(flowmesh.data.vertices)
+		tempvlocs = [(v.co).copy() for v in flowmesh.data.vertices]
 		
 		bpy.ops.object.particle_system_add()
-		psettings = context.active_object.particle_systems[0].settings
+		psettings = flowmesh.particle_systems[0].settings
 		psettings.count = numverts
 		psettings.emit_from = 'VERT'
 		psettings.normal_factor = 0.0
@@ -86,126 +118,265 @@ class create_flowmap_field(bpy.types.Operator):
 		psettings.use_dynamic_rotation = True
 		psettings.effector_weights.gravity = 0.0
 		
+		flowmesh.particle_systems[0].point_cache.name = 'ForceFlow'
+		
+		context.area.tag_redraw()
+		context.scene.update()
+		
+		flowmap_data.flow_startlocs = [(p.location).copy() for p in flowmesh.particle_systems[0].particles]
+		flowmap_data.flow_vindex = [get_nearest_vindex(tempvlocs, v) for v in flowmap_data.flow_startlocs]
+		
+		bpy.data.materials.new('flowmat')
+		bpy.data.materials['flowmat'].use_shadeless = True
+		bpy.data.materials['flowmat'].use_vertex_color_paint = True
+		
+		flowmesh.active_material = bpy.data.materials['flowmat']
+		
+		del tempvlocs[:]
+		
 		return {'FINISHED'}
 
 
-# calculates velocities and saves them to vertex colors
-class calc_flowmap_velocicites(bpy.types.Operator):
-	bl_idname = 'object.calc_flowmap_velocicites'
-	bl_label = 'Calculate Flow'
-	bl_description = 'Calculate and save velocities'
-	bl_options = {'REGISTER', 'UNDO'}
 
+## Calc ####################################################
+
+# Base (Painted directions)
+class calc_flowmap_dir(bpy.types.Operator):
+	bl_idname = 'object.calc_flowmap_dir'
+	bl_label = 'Base Flow'
+	bl_description = 'Get base directional flow'
+	bl_options = {'REGISTER', 'UNDO'}
+	
 	@classmethod
 	def poll(cls, context):
-		return (context.mode == "OBJECT" and context.active_object != None) and len(context.active_object.data.particle_systems) > 0
+		if (context.mode == "OBJECT" and context.active_object != None):
+			if context.active_object.particle_systems:
+				return len(context.active_object.particle_systems) > 0
+		return False
 	
 	def execute(self, context):
 		flowmesh = context.active_object
+		pscurlocs = [(p.location).copy() for p in flowmesh.particle_systems[0].particles]
+		
+		flowmap_data.flow_directions = []
+		for i in range(len(flowmap_data.flow_startlocs)):
+			flowmap_data.flow_directions.append(pscurlocs[i] - flowmap_data.flow_startlocs[i])
+		
+		return {'FINISHED'}
 
-		particleslist = [p.velocity for p in flowmesh.particle_systems[0].particles]
 
+# Forces (flow velocities influenced by force fields)
+class calc_flowmap_velocities(bpy.types.Operator):
+	bl_idname = 'object.calc_flowmap_velocities'
+	bl_label = 'Force Flow'
+	bl_description = 'Get flow influenced by forces'
+	bl_options = {'REGISTER', 'UNDO'}
+	
+	@classmethod
+	def poll(cls, context):
+		if (context.mode == "OBJECT" and context.active_object != None):
+			if context.active_object.particle_systems:
+				return len(context.active_object.particle_systems) > 0
+		return False
+	
+	def execute(self, context):
+		flowmesh = context.active_object
+		flowmap_data.flow_velocities = [(p.velocity).copy() for p in flowmesh.particle_systems[0].particles]
+		
+		return {'FINISHED'}
+
+
+# Geometry (flow velocities influenced by level geometry)
+class calc_flowmap_geometry(bpy.types.Operator):
+	bl_idname = 'object.calc_flowmap_geometry'
+	bl_label = 'Geometry Flow'
+	bl_description = 'Get flow influenced by surrounding geometry'
+	bl_options = {'REGISTER', 'UNDO'}
+	
+	@classmethod
+	def poll(cls, context):
+		if (context.mode == "OBJECT" and context.active_object != None):
+			if context.active_object.particle_systems:
+				return len(context.active_object.particle_systems) > 0
+		return False
+	
+	def execute(self, context):
+		flowmesh = context.active_object
+		
+		# 	for each selected mesh
+		#		flocnorms = get faces [loc + normal]
+		#		for each influenced particle
+		#			get faces in influencedistance
+		#			average newv and weighted face normal
+		
+		return {'FINISHED'}
+
+
+
+## Write ####################################################
+
+# combine flow influences + write to vertex color layer
+class flowmap_writetocolor(bpy.types.Operator):
+	bl_idname = 'object.flowmap_writetocolor'
+	bl_label = 'Write VColors'
+	bl_description = 'Write flowmap data to vertex color layer'
+	bl_options = {'REGISTER', 'UNDO'}
+	
+	@classmethod
+	def poll(cls, context):
+		if (context.mode == "OBJECT" and context.active_object != None):
+			if context.active_object.particle_systems:
+				return len(context.active_object.particle_systems) > 0
+		return False
+	
+	def execute(self, context):
+		flowmesh = context.active_object
+		flowmeshloops = [f for f in flowmesh.data.loops]
+		loopslen = len(flowmeshloops)
+		finalvlist = [Vector([0.0,0.0,0.0]) for l in flowmeshloops]
 		newlayer = None
-
+		
+		c0weight = context.window_manager.flowmap_cache0weight
+		c1weight = context.window_manager.flowmap_cache1weight
+		c2weight = context.window_manager.flowmap_cache2weight
+		
+		# direction
+		if c0weight > 0.0:
+			if len(flowmap_data.flow_directions) > 0:
+				for i in range(len(flowmap_data.flow_vindex)):
+					for j in range(len(flowmeshloops)):
+						if flowmeshloops[j].vertex_index == flowmap_data.flow_vindex[i]:
+							finalvlist[j] = flowmap_data.flow_directions[i] * c0weight
+		
+		# forces
+		if c1weight > 0.0:
+			if len(flowmap_data.flow_velocities) > 0:
+				for i in range(len(flowmap_data.flow_vindex)):
+					for j in range(len(flowmeshloops)):
+						if flowmeshloops[j].vertex_index == flowmap_data.flow_vindex[i]:
+							finalvlist[j] = finalvlist[j] + (flowmap_data.flow_velocities[i] * c1weight)
+		
+		# geometry
+		if c2weight > 0.0:
+			if len(flowmap_data.flow_geominf) > 0:
+				for i in range(len(flowmap_data.flow_vindex)):
+					for j in range(len(flowmeshloops)):
+						if flowmeshloops[j].vertex_index == flowmap_data.flow_vindex[i]:
+							finalvlist[j] = finalvlist[j] + (flowmap_data.flow_geominf[i] * c2weight)
+		
+		for v in finalvlist:
+			if v.magnitude > 0.0:
+				v = v.normalized()
+		
+		# check if vertex color layer exists, create one if not
 		if len(flowmesh.data.vertex_colors) < 1:
 			bpy.ops.mesh.vertex_color_add()
 			newlayer = flowmesh.data.vertex_colors[len(flowmesh.data.vertex_colors) - 1]
 			newlayer.name = 'flow'
 		else:
 			newlayer = flowmesh.data.vertex_colors['flow']
-
-		finalvlist = [Vector([0.0,0.0,0.0]) for l in flowmesh.data.loops]
-
-		for i in range(len(flowmesh.data.loops)):
-			finalvlist[i] = particleslist[flowmesh.data.loops[i].vertex_index]
-
+		
+		# convert + write vertex colors
 		if newlayer != None:
-			for i in range(len(finalvlist)):
-				newlayer.data[i].color = (finalvlist[i] + Vector([1.0,1.0,1.0])) / 2.0
-
+			if context.window_manager.flowmap_usebchan:
+				for i in range(len(finalvlist)):
+					newlayer.data[i].color = (finalvlist[i] + Vector([1.0,1.0,1.0])) / 2.0
+			else:
+				for i in range(len(finalvlist)):
+					newlayer.data[i].color = (finalvlist[i] + Vector([1.0,1.0,0.0])) / 2.0
+					newlayer.data[i].color[2] = 0.0
+		
 		return {'FINISHED'}
+		
 
+## PP ######################################################
 
-# creates particle system on selected plane to represent flowmap vectors
-class blur_flowmap_field(bpy.types.Operator):
-	bl_idname = 'object.blur_flowmap_field'
-	bl_label = 'Blur Flowmap'
-	bl_description = 'Blurs the directions of the flowmap by averaging surrounding velocities'
+# blur existing flowmap
+class flowmap_pp_blur(bpy.types.Operator):
+	bl_idname = 'object.flowmap_pp_blur'
+	bl_label = 'Smooth'
+	bl_description = 'Blur current velocity lists'
 	bl_options = {'REGISTER', 'UNDO'}
-
+	
 	@classmethod
 	def poll(cls, context):
-		return (context.mode == "OBJECT" and context.active_object != None)
+		if (context.mode == "OBJECT" and context.active_object != None):
+			if context.active_object.particle_systems:
+				return len(context.active_object.particle_systems) > 0
+		return False
 	
 	def execute(self, context):
-		flowmesh = context.active_object
-		
-		maxradius = 1.0
-		intensity = 0.5
-		
-		vindex = [v.vertex_index for v in flowmesh.data.loops]
-		vlist = [v.co for v in flowmesh.data.vertices]
-		
-		particleslist_unproc = [p.velocity.copy() for p in flowmesh.particle_systems[0].particles]
-		particleslist_proc = [p.velocity.copy() for p in flowmesh.particle_systems[0].particles]
-		
-		curavglist = []
-		tempv = Vector([0.0,0.0,0.0])
-		
-		for j in range(len(vindex)):
-			v1 = vlist[vindex[j]]
-			
-			for i in range(len(vlist)):
-				curdistv = v1 - vlist[i]
-				curdist = curdistv.magnitude
-				if curdist <= maxradius:
-					curavglist.append(particleslist_unproc[i])
-			
-			if len(curavglist) > 0:
-				for v in curavglist:
-					tempv = tempv + v
-				tempv = tempv / float(len(curavglist))
-				particleslist_proc[vindex[j]] = tempv.copy()
-			
-			curavglist.clear()
-			tempv.zero()
-			
-		newlayer = flowmesh.data.vertex_colors['flow']
-		if newlayer != None:
-			for i in range(len(newlayer.data)):
-				newlayer.data[i].color = (particleslist_proc[vindex[i]] + Vector([1.0,1.0,1.0])) / 2.0
-		
 		return {'FINISHED'}
 
+
+
+#######################################################################
+
+def initdefaults():
+	bpy.types.WindowManager.flowmap_usebchan = bpy.props.BoolProperty(
+		default=False,description="Use blue channel (height)"
+	)
+	bpy.types.WindowManager.flowmap_cache0weight = bpy.props.FloatProperty(
+		default=0.5,min=0.0,max=1.0,
+		description="Weight of directional flow in final calculation"
+	)
+	bpy.types.WindowManager.flowmap_cache1weight = bpy.props.FloatProperty(
+		default=0.25,min=0.0,max=1.0,
+		description="Weight of forces in final calculation"
+	)
+	bpy.types.WindowManager.flowmap_cache2weight = bpy.props.FloatProperty(
+		default=0.25,min=0.0,max=1.0,
+		description="Weight of geometry influence in final calculation"
+	)
+
+
+def clearvars():
+	props = [
+		'flowmap_usebchan','flowmap_cache0weight','flowmap_cache1weight','flowmap_cache2weight'
+	]
+	
+	for p in props:
+		if bpy.context.window_manager.get(p) != None:
+			del bpy.context.window_manager[p]
+		try:
+			x = getattr(bpy.types.WindowManager, p)
+			del x
+		except:
+			pass
+	
+	flowmap_data.clear_flowmapdata(context)
 
 
 def register():
+	initdefaults()
+	
 	bpy.utils.register_class(flowmaps_editor_panel)
 	
-	bpy.utils.register_class(create_flowmap_field)
-	bpy.utils.register_class(calc_flowmap_velocicites)
-	bpy.utils.register_class(blur_flowmap_field)
+	bpy.utils.register_class(create_vectorfield_2d)
 	
-	#initdefaults()
+	bpy.utils.register_class(calc_flowmap_dir)
+	bpy.utils.register_class(calc_flowmap_velocities)
+	bpy.utils.register_class(calc_flowmap_geometry)
+	
+	bpy.utils.register_class(flowmap_writetocolor)
+	
+	
+	
+	
 
 def unregister():
 	bpy.utils.unregister_class(flowmaps_editor_panel)
 	
-	bpy.utils.unregister_class(create_flowmap_field)
-	bpy.utils.unregister_class(calc_flowmap_velocicites)
-	bpy.utils.unregister_class(blur_flowmap_field)
+	bpy.utils.unregister_class(create_vectorfield_2d)
 	
-	#clearvars(bpy)
-
-
-#def initdefaults(bpy):
-#	#  
-
-
-#def clearvars(bpy):
-#	#  
-
-
+	bpy.utils.unregister_class(calc_flowmap_dir)
+	bpy.utils.unregister_class(calc_flowmap_velocities)
+	bpy.utils.unregister_class(calc_flowmap_geometry)
+	
+	bpy.utils.unregister_class(flowmap_writetocolor)
+	
+	clearvars()
+	
 
 if __name__ == '__main__':
 	register()
--- README.md
+++ README.md
@@ -19,21 +19,20 @@ The UI panel is in the Particle Simulation tab.
 - Duplicate the mesh you want to create a flowmap for (or a basic plane for tiling water textures)
   - Note: the mesh needs UVs to be able to bake the flowmap to texture
 - Subdivide the copy a few times for more particles in the flow simulation
-- Click Create in the UI panel with the new mesh selected. This creates the particle system.
+- Click *Create Data* in the UI panel with the new mesh selected. This creates the particle system.
   
 - Add any force fields you want to influence the flow simulation
   - ex. any places that the flow will be diverted at, low-range wind forces at the edges to simulate a shore, etc
-- select the frame of the simulation you want to get velocities from
-- Click Calculate. This will create a new vertex color layer called "flow"
-
-- Use Blender's Bake feature to bake the vertex colors to a texture 
- 
-- Save the image.
+- select the frame of the simulation you want to get velocities from  
   
-*(Optional:)*
-- Click Blur instead of Calculate if you want to try the unfinished blur filter 
-  - calculates the flowmap and applies a 2.0 unit distance per-vertex average blur to the result
-  - it's better at the moment to just open the resulting image in an image editor and apply any blurring you want from there.
+- To calculate the velocities for the flowmap:
+  - Click Calc Direction to use the amount by which each particle moved between frame 0 and the current frame
+  - (or) Calc Forces to use each particle's actual velocity in this frame
+  - (Calc Geometry does nothing for now)
+  - This will create a new vertex color layer called "flow"  
+  
+- Use Blender's Bake feature to bake the vertex colors to a texture 
+- Save the image from UV view.  
  
  
 - The resulting flowmap is packed in the same way a normal map would be, so the values will have to be converted in the engine you're using.
